d56dc40064e8c6adf6ceb42f0453503699cd152f
alice <122629939+espcaa@users.noreply.github.com>
2026-02-21 15:31:13 -0800

    new camera thing but broken

--- ball.gd
+++ ball.gd
@@ -3,7 +3,13 @@ extends RigidBody3D
 @onready var gpu_particles_3d: CPUParticles3D = $GPUParticles3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+
 func blow_up():
 	gpu_particles_3d.emitting = true
 	animation_player.play("blow_up_more")
-	
+
+
+func _physics_process(_delta: float) -> void:
+	# if we have a low speed, apply a random impulse only on x/y (not z)
+	if linear_velocity.length() < 0.1:
+		apply_impulse(Vector3(randf_range(-10, 10), randf_range(-10, 10), 0))
--- main_script.gd
+++ main_script.gd
@@ -3,9 +3,8 @@ extends Node3D
 @onready var UI = get_tree().get_first_node_in_group("UI")
 
 @onready var cameras: Node3D = $Cameras
-@onready var camera_pivot: Node3D = $Cameras/CameraPivot
-@onready var camera_pivot_camera: Camera3D = $Cameras/CameraPivot/CameraPivotCamera
 @onready var follow_ball_cam: Camera3D = $Cameras/FollowPoint/FollowBallCam
+@onready var camera_pivot = $Cameras/FollowPoint
 
 @export var cam_rotate_speed = 0.5
 @export var ball_impulse = 0.5
@@ -51,26 +50,9 @@ func spawn_ball():
 	)
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("restart"):
 		get_tree().reload_current_scene()
-
-	camera_pivot.rotation.y += cam_rotate_speed * delta * cam_direction
-	follow_ball_cam.position.z = current_ball.position.z + 20
-
-	# rotate the camera to always look at the ball, use a tween + raycast to check for obstacles
-	var raycast = PhysicsRayQueryParameters3D.create(
-		current_ball.position, follow_ball_cam.position
-	)
-	raycast.collide_with_areas = true
-	# only collide with layer #2 (walls)
-	raycast.collision_mask = 1 << 2
-	raycast.collide_with_bodies = true
-	var space_state = get_world_3d().direct_space_state
-	var result = space_state.intersect_ray(raycast)
-	if result:
-		# the raycast hit something so we move $Cameras/FollowPoint.rotation.y
-		$Cameras/FollowPoint.rotation.y += cam_rotate_speed * delta * cam_direction
 
 
 #func _input(_event: InputEvent) -> void:
@@ -87,17 +69,6 @@ func goal(body: Node3D, player_num: int):
 		super_fast_mode = true
 	else:
 		super_fast_mode = false
-
-	# set all cameras to not be current
-	camera_pivot_camera.current = false
-	follow_ball_cam.current = false
-
-	# chooses a random camera to switch to
-	match randi_range(0, cameras.get_child_count()):
-		0:
-			camera_pivot_camera.current = true
-		1:
-			follow_ball_cam.current = true
 
 
 func _on_p_1_goal_body_entered(body: Node3D) -> void:
